c52316019ceaa8e1a24ef9145d5e002ffd099db5
aiharshh <158760777+aiharshh@users.noreply.github.com>
2024-06-07 08:36:22 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 746
+let accountEmail = "harsh@google.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+// accountId = 2  Not allowed
+accountEmail = "harsh@nvdia.com"
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as a newer version
+
+// alert("Hello")  // We are using node js and not browser
+
+console.log(3 
+    + 
+    3
+);  // Don't do this, Code readability matters!!
+
+console.log("HARSH")
+
+let name = "Harsh"
+let age = 18
+let isLoggedIn = false
+
+// symbol => Unique
+
+// Object 
+
+console.log(typeof null);
